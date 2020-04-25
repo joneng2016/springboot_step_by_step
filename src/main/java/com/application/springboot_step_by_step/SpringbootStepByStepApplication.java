f140099e40b97e2a8e09f3bfe289aaf7d887fad9
Jonathan Morris Samara <jonathan_samara@hotmail.com>
2020-04-25 15:17:48 -0300

    commit application

---
+++ src/main/java/com/application/springboot_step_by_step/SpringbootStepByStepApplication.java
@@ -0,0 +1,13 @@
+package com.application.springboot_step_by_step;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootStepByStepApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootStepByStepApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/application/springboot_step_by_step/SpringbootStepByStepApplicationTests.java
@@ -0,0 +1,13 @@
+package com.application.springboot_step_by_step;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootStepByStepApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
